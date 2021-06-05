66119af462e23ba5d3bb3cf5eeb227a879d87fcb
akın ayhan <akinayhan@hotmail.com.tr>
2021-06-06 00:56:51 +0300

    06.06

--- src/App.js
+++ src/App.js
@@ -4,14 +4,14 @@ import 'semantic-ui-css/semantic.min.css'
 import { Container } from 'semantic-ui-react';
 import Navi from './layouts/Navi';
 
+
 function App() {
   return (
     <div className="App">
       <Navi/>
       <Container className="main">
-      <Dashboard/>
-      </Container>
-      
+         <Dashboard/>
+      </Container>    
     </div>
   );
 }
